ba58a5f6e58ffe748743d11d1c661fc82435be4f
Eric Z <ericzh509@gmail.com>
2025-10-16 19:44:46 -0500

    Updated sponsors page

--- client/src/components/sponsors/sponsors_list.ts
+++ client/src/components/sponsors/sponsors_list.ts
@@ -22,6 +22,36 @@ const sponsorsList = [
         SponsorImage: "../../../sponsors/tms.png",
         SponsorWebsite: "https://tmstitanium.com/",
         Status: "Current"
+    },
+    {
+        Name: "Sorf",
+        SponsorImage: "../../../sponsors/sorf.png",
+        SponsorWebsite: "https://www.instagram.com/illinoissorf/?hl=en",
+        Status: "Current"
+    },
+    {
+        Name: "Repeat Robotics",
+        SponsorImage: "../../../sponsors/repeatrobotics.png",
+        SponsorWebsite: "https://repeat-robotics.com/",
+        Status: "Current"
+    },
+    {
+        Name: "DMC",
+        SponsorImage: "../../../sponsors/dmc.png",
+        SponsorWebsite: "https://www.dmcinfo.com/",
+        Status: "Current"
+    },
+    {
+        Name: "EPlastics",
+        SponsorImage: "../../../sponsors/eplastics.png",
+        SponsorWebsite: "https://www.eplastics.com/",
+        Status: "Current"
+    },
+    {
+        Name: "Anduril",
+        SponsorImage: "../../../sponsors/anduril.png",
+        SponsorWebsite: "https://www.anduril.com/",
+        Status: "Current"
     }
     
 ]
